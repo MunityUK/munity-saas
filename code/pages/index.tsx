080a8e08a54044bc6d923a61c954a9afdbbf0e30
Zavid <zavidegbue@gmail.com>
2021-04-10 01:34:10 +0100

    Strictly type TypeScript

--- code/pages/index.tsx
+++ code/pages/index.tsx
@@ -3,15 +3,15 @@ import { useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  useEffect(() => {
+    getCrimes();
+  }, []);
+
   const getCrimes = async () => {
     const res = await fetch('/api/crimes');
     const records = await res.json();
     console.log(records);
   };
-
-  useEffect(() => {
-    getCrimes();
-  }, []);
 
   return (
     <div className={styles.container}>
@@ -20,19 +20,10 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}>
-        
-      </main>
+      <main className={styles.main}></main>
 
       <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by Voicera
-          <img src='/voicera.svg' alt='Voicera Logo' className={styles.logo} />
-        </a>
+        <img src='/voicera.svg' alt='Voicera Logo' className={styles.logo} />
       </footer>
     </div>
   );
