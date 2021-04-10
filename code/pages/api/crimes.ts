8434b5cb5b7e16b5bab8b96f7e0171badb33d0b2
Zavid <zavidegbue@gmail.com>
2021-04-10 01:45:47 +0100

    Error handling of crimes request

--- code/pages/api/crimes.ts
+++ code/pages/api/crimes.ts
@@ -4,6 +4,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const TABLE_NAME = 'Table 1';
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
-  const records = await airtable(TABLE_NAME).select().all();
-  res.status(200).json({ records });
+  try {
+    const records = await airtable(TABLE_NAME).select().all();
+    res.status(200).json({ records });
+  } catch (err) {
+    const { message, statusCode } = err;
+    res.status(statusCode).json({ message });
+  }
 };
