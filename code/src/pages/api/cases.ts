5ee257c2ba14ee401e938d534f72a0136548abed
Zavid <zavidegbue@gmail.com>
2021-05-02 01:27:31 +0100

    Allow creating cases

--- code/src/pages/api/cases.ts
+++ code/src/pages/api/cases.ts
@@ -4,10 +4,15 @@ import airtable from 'src/utils/airtable';
 
 const TABLE_NAME = 'Cases';
 
-export default async function (_req: NextApiRequest, res: NextApiResponse) {
+export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
-    const records = await getCases();
-    res.status(200).json({ records });
+    if (req.method === 'GET') {
+      const records = await getCases();
+      res.status(200).json({ records });
+    } else if (req.method === 'POST') {
+      const records = await createCases();
+      res.status(201).json({ records });
+    }
   } catch (err) {
     const { message, statusCode } = err;
     res.status(statusCode).json({ message });
@@ -15,6 +20,32 @@ export default async function (_req: NextApiRequest, res: NextApiResponse) {
 }
 
 export async function getCases() {
-  const records = airtable(TABLE_NAME).select().all();
-  return records;
+  return airtable(TABLE_NAME).select().all();
+}
+
+export async function createCases() {
+  return airtable(TABLE_NAME).create([
+    {
+      fields: {
+        'Start date': '2020-10-07',
+        Month: 'October',
+        Year: '2020',
+        'End date': '2020-10-07',
+        'Date recorded by us': '2020-10-08',
+        Demographic: 'Black ',
+        'Incident type': 'Harrasment',
+        'Coordinates Type': 'Standard',
+        Country: 'United Kingdom',
+        Region: 'South West',
+        Borough: 'Easton',
+        'City/Town': 'Bristol',
+        'Court/Police report number': 1223423,
+        Sources: 'www.bristolpost.co.uk',
+        Description: 'Black Woman assualted by Bristol police',
+        'Perpetrator (Station)': 'Trinity Police Station ',
+        'Number of officers involved': 2,
+        'Officer IDs': ['24B', '24C']
+      }
+    }
+  ]);
 }
