3c6151c807e37bb5f3ae9e95ddd0dd5be9a00126
Zavid <zavidegbue@gmail.com>
2021-05-31 17:18:59 +0100

    Change everything back

--- code/utils/knex.ts
+++ code/utils/knex.ts
@@ -11,9 +11,9 @@ export const DB_TABLE_TEST = 'complaints_test';
 export default Knex({
   client: 'mysql2',
   connection: {
-    host: 'localhost',
-    user: 'voicera',
-    password: 'voicera',
-    database: 'voicera'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   }
 });
