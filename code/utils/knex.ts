0bb0898aa4729a1777acecd4a08a0d9aa4de156b
Zavid <zavidegbue@gmail.com>
2021-05-31 17:16:49 +0100

    Add back better envvars

--- code/utils/knex.ts
+++ code/utils/knex.ts
@@ -12,8 +12,8 @@ export default Knex({
   client: 'mysql2',
   connection: {
     host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'complaints_test'
+    user: 'voicera',
+    password: 'voicera',
+    database: 'voicera'
   }
 });
