804d83d95231371b1b9a2e09e670bce1fe278a10
Zavid <zavidegbue@gmail.com>
2021-07-17 19:33:38 +0100

    Style day names

--- code/src/components/form/lib/datepicker.tsx
+++ code/src/components/form/lib/datepicker.tsx
@@ -5,8 +5,10 @@ export function DatePicker({ placeholderText }: DatePickerProps) {
   const [startDate, setStartDate] = useState<any>(new Date());
   return (
     <ReactDatePicker
+      calendarStartDay={1}
       dateFormat={'dd/MM/yyyy'}
       dropdownMode={'select'}
+      formatWeekDay={(day) => day.substr(0, 3)}
       isClearable={true}
       maxDate={new Date()}
       onChange={(date) => setStartDate(date)}
