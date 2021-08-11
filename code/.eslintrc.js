b86a26d575c6dbbbe384f224aa190e5c3f22e497
Zavid <zavidegbue@gmail.com>
2021-08-12 00:19:25 +0100

    Remove extra tsconfig

--- code/.eslintrc.js
+++ code/.eslintrc.js
@@ -1,11 +1,4 @@
 module.exports = {
   extends: '@zzavidd/eslint-config',
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: [
-      './tsconfig.eslint.json'
-    ],
-    tsconfigRootDir: __dirname
-  },
   ignorePatterns: ['**/*.js', 'next-env.d.ts']
 };
