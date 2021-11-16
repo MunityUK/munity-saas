7e22c9241d7687020bbb4050e4d35229f9e431d8
Zavid <zavidegbue@gmail.com>
2021-11-16 23:49:10 +0000

    Amalgamate tryCatch and run utils functions

--- code/cli/test/database.test.ts
+++ code/cli/test/database.test.ts
@@ -11,27 +11,32 @@ import { describe, it } from 'mocha';
 import { conn } from '../src/config';
 
 describe('Database Tests', function () {
-  it(
-    'Create test table',
-    MunityTest.tryCatch(async () => {
-      await conn.createTable(DB_TABLE_TEST);
-      const tableExists = await conn.conn.schema
-        .withSchema(DB_SCHEMA)
-        .hasTable(DB_TABLE_TEST);
-      assert.isTrue(tableExists, `Expected table '${DB_TABLE_TEST}' to exist.`);
-    })
-  );
+  it('Create test table', () =>
+    MunityTest.run(
+      async () => {
+        await conn.createTable(DB_TABLE_TEST);
+        const tableExists = await conn.conn.schema
+          .withSchema(DB_SCHEMA)
+          .hasTable(DB_TABLE_TEST);
+        assert.isTrue(
+          tableExists,
+          `Expected table '${DB_TABLE_TEST}' to exist.`
+        );
+      },
+      { assertFail: true }
+    ));
 
-  it(
-    'Ingest data into test table',
-    MunityTest.tryCatch(async () => {
-      await conn.insertRecords(
-        DB_TABLE_TEST,
-        Complaint.create({ status: ComplaintStatus.RESOLVED })
-      );
-      const res = await conn.conn(DB_TABLE_TEST).count();
-      const size = res[0]['count(*)'];
-      assert.equal(size, 1);
-    })
-  );
+  it('Ingest data into test table', () =>
+    MunityTest.run(
+      async () => {
+        await conn.insertRecords(
+          DB_TABLE_TEST,
+          Complaint.create({ status: ComplaintStatus.RESOLVED })
+        );
+        const res = await conn.conn(DB_TABLE_TEST).count();
+        const size = res[0]['count(*)'];
+        assert.equal(size, 1);
+      },
+      { assertFail: true }
+    ));
 });
--- code/utils/src/functions/test.ts
+++ code/utils/src/functions/test.ts
@@ -1,39 +1,30 @@
 import { assert } from 'chai';
 
 /**
- * Executes a specified function and exits the program.
+ * Executes a specified function and handles any thrown errors.
  * @param main The function to execute.
  * @returns A promise.
  */
 export async function run(
-  main: () => Promise<void>,
+  main: () => Promise<void | never>,
   options: RunOptions = {}
 ): Promise<void | never> {
-  const { exit = false } = options;
+  const { assertFail = false, exit = false } = options;
   try {
     await main();
   } catch (err: any) {
+    if (assertFail) {
+      assert.fail(err);
+    }
     console.error(`${err.name}: ${err.message}`);
   } finally {
     if (exit) process.exit(0);
   }
 }
 
-/**
- * Runs a sequence of instructions prone to throwing an exception.
- * @param runnable The runnable sequence of instructions.
- * @returns The function surrounded in a try-catch.
- */
-export function tryCatch(runnable: () => Promise<void>) {
-  return async () => {
-    try {
-      await runnable();
-    } catch (err: any) {
-      assert.fail(err);
-    }
-  };
-}
-
 type RunOptions = {
+  /** Should assert fail with error if the function fails. Usually used for tests. */
+  assertFail?: boolean;
+  /** Should exit the process after running. */
   exit?: boolean;
 };
